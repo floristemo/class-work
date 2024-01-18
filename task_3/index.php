2fc77ffc6546bf67e33ab8eaf6e620888f7a159b
floristemo <vadimsmir1@ya.ru>
2024-01-18 14:38:15 +0300

    Сделано 1 задание в task_3

---
+++ task_3/index.php
@@ -0,0 +1,10 @@
+<?php
+for ($number = 1; $number <= 10; $number++) {
+    if ($number == 1) {
+        echo $number;
+    } else if ($number == 10) {
+        echo "-$number";
+    } else {
+        echo "-$number";
+    }
+}
