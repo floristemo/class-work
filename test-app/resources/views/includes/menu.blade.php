d7d961bee1219541ee60390b2d1c477465a571b9
floristemo <vadimsmir1@ya.ru>
2024-01-26 18:59:14 +0300

    Все нормально сделал все работает

---
+++ test-app/resources/views/includes/menu.blade.php
@@ -0,0 +1,17 @@
+<ul>
+    <li>
+        <a href="{{ route('home') }}">{{ __('Главная') }}</a>
+    </li>
+    <li>
+        <a href="{{ route('about') }}">{{ __('О нас') }}</a>
+    </li>
+    <li>
+        <a href="{{ route('post.index') }}">{{ __('Все записи') }}</a>
+    </li>
+    <li>
+        <a href="{{ route('category.index') }}">{{ __('Все категории') }}</a>
+    </li>
+    <li>
+        <a href="{{ route('tovar.index') }}">{{ __('Все товары') }}</a>
+    </li>
+</ul>
---
+++ test-app/resources/views/tovar/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <a href="{{ route('tovar.create') }}" class="btn btn-primary">Новый товар</a>
+        </div>
+        <div class="row">
+            @if($message = Session::get('success'))
+                <div class="alert alert-success">
+                    {{ $message }}
+                </div>
+            @endif
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Название поста</th>
+                    <th scope="col">Описание поста</th>
+                    <th scope="col">Изображение</th>
+                    <th scope="col">Действие</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse($products as $product)
+                    <tr>
+                        <th scope="row">
+                            {{ $product->id }}
+                        </th>
+                        <td>
+                            {{ $product->name }}
+                        </td>
+                        <td>
+                            {{ $product->text }}
+                        </td>
+                        <td>
+                            {{ $product->category->name }}
+                        </td>
+                        <td>
+                            @if ($product->is_active == 0)
+                                <div class="alert alert-danger text-center">
+                                    {{ __('Не активна') }}
+                                </div>
+                            @else
+                                <div class="alert alert-success text-center">
+                                    {{ __('Активна') }}
+                                </div>
+                            @endif
+                        </td>
+                    </tr>
+                @empty
+                    Данных нет
+                @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
